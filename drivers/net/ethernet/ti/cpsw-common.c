41a4e64b7c00910e6d9502fab23dc88b9afdf625
Michael Gloff <mgloff@emacinc.com>
2017-04-26 18:47:27 -0500

    Hard code the SoM-3517 ethernet slave to 0.
    Signed-off-by: Michael Gloff <mgloff@emacinc.com>

--- drivers/net/ethernet/ti/cpsw-common.c
+++ drivers/net/ethernet/ti/cpsw-common.c
@@ -85,7 +85,7 @@ int ti_cm_get_macid(struct device *dev, int slave, u8 *mac_addr)
 		return cpsw_am33xx_cm_get_macid(dev, 0x630, slave, mac_addr);
 
 	if (of_device_is_compatible(dev->of_node, "ti,am3517-emac"))
-		return davinci_emac_3517_get_macid(dev, 0x110, slave, mac_addr);
+		return davinci_emac_3517_get_macid(dev, 0x110, 0, mac_addr);
 
 	if (of_device_is_compatible(dev->of_node, "ti,dm816-emac"))
 		return cpsw_am33xx_cm_get_macid(dev, 0x30, slave, mac_addr);
