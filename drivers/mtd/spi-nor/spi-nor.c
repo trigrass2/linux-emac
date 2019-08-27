1a63df2c7db5a777eb35ef7f068b8c6bca9d6d9f
Michael Gloff <mgloff@emacinc.com>
2019-08-27 16:36:08 -0500

    spi-nor.c: Add support for fm25v20a FRAM.
    Signed-off-by: Michael Gloff <mgloff@emacinc.com>

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -1008,6 +1008,7 @@ static const struct flash_info spi_nor_ids[] = {
 	{ "w25q80bl", INFO(0xef4014, 0, 64 * 1024,  16, SECT_4K) },
 	{ "w25q128", INFO(0xef4018, 0, 64 * 1024, 256, SECT_4K) },
 	{ "w25q256", INFO(0xef4019, 0, 64 * 1024, 512, SECT_4K) },
+	{ "fm25v20a",  INFO(0x7f7f7f, 0,        256, 1024, 0) },
 
 	/* Catalyst / On Semiconductor -- non-JEDEC */
 	{ "cat25c11", CAT25_INFO(  16, 8, 16, 1, SPI_NOR_NO_ERASE | SPI_NOR_NO_FR) },
